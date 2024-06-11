a9c224cbac17621380708e8119d4ee27d61e2062
Muskan Swarup <muskanswarup680@gmail.com>
2024-06-11 11:03:03 +0530

    completed

--- script.js
+++ script.js
@@ -3,58 +3,95 @@ let addButton = document.getElementById("button");
 let todos = document.querySelector(".todos");
 let errorMessage = document.querySelector(".error");
 let listItems = document.querySelectorAll(".myList ul li");
+let removeBtns = document.querySelectorAll(".close");
+let completedBtns = document.querySelectorAll(".tickMark")
+
+addButton.addEventListener("click", () => {
+  console.log("YOu clicked me?");
+  errorMessage.innerText = "";
+
+  let inputValue = input.value;
+
+  if (inputValue.trim() !== "") {
+    let li = document.createElement("li");
+
+    let tickMark = document.createElement("span");
+
+    tickMark.className = "tickMark";
+    tickMark.innerHTML = "&#10004";
+    tickMark.style.display = "none";
+    li.appendChild(tickMark);
+
+    li.appendChild(document.createTextNode(`${inputValue}`));
+
+    let span = document.createElement("span");
+    span.className = "close";
+    span.innerHTML = "&#10006";
+    li.appendChild(span);   
 
 
-addButton.addEventListener("click" , () => {
-    console.log("YOu clicked me?");
-    errorMessage.innerText = "";
+    let ul = document.querySelector(".myList ul");
+    ul.appendChild(li);
 
-    let inputValue = input.value;
+    inputValue = "";
 
-    if(inputValue.trim() !== ''){
-        let li = document.createElement("li");
+    span.addEventListener("click" , () => {
+        li.remove();
+    })
 
-        li.textContent = inputValue;
+    li.addEventListener("click", () => {
+        if (li.style.textDecoration === "line-through") {
+          li.style.textDecoration = "none";
+          li.style.backgroundColor = "";
+          li.className = "checked";
+          tickMark.style.display = "none"; 
+        } else {
+          li.style.textDecoration = "line-through";
+          li.style.backgroundColor = "black";
+          li.style.color = "white";
+          tickMark.style.display = "inline"; 
+        }
+      });
 
-        let ul = document.querySelector(".myList ul");
-        ul.appendChild(li);
+    input.value = "";
+  } else {
+    console.log("Value must be provided");
 
-        input.value = "";
-    }else{
-        console.log("Value must be provided");
+    let para = document.createElement("p");
+    para.innerText = "You must provide a value";
+    para.style.fontSize = "1.5rem";
+    para.style.color = "red";
 
-        let para = document.createElement("p");
-        para.innerText = "You must provide a value";
-        para.style.fontSize = "1.5rem";
-        para.style.color = "red";
+    errorMessage.appendChild(para);
+  }
+});
 
-        errorMessage.appendChild(para);
-        // todos.appendChild(errorMessage);
-       
+
+
+listItems.forEach((item) => {
+
+  item.addEventListener("click", () => {
+
+    let tickMark = item.querySelector(".tickMark");
+
+    if (item.style.textDecoration === "line-through") {
+      item.style.textDecoration = "none";
+      item.style.backgroundColor = "";
+      item.className = "checked";
+      tickMark.style.display = "none";
+    } else {
+      item.style.textDecoration = "line-through";
+      item.style.backgroundColor = "black";
+      item.style.color = "white";
+      tickMark.style.display = "inline";      
     }
+  });
+});
+
+removeBtns.forEach( (removeBtn)=> {
+    removeBtn.addEventListener("click" , () => {
+        console.log("remove btn was clicked");
+        removeBtn.parentElement.remove();
+    })
 })
 
-
-
-    listItems.forEach(item => {
-
-        let className = item.getAttribute("class");
-        if(className === "checked"){
-            console.log(item.textContent);
-            item.style.textDecoration = "line-through";    
-            item.style.backgroundColor = "black";
-        }
-
-        item.addEventListener("click" , () => {
-            // item.style.textDecoration = "none";
-
-            if(item.style.textDecoration === "line-through"){
-                item.style.textDecoration = "none";
-                item.style.backgroundColor = ""
-            }else{
-                item.style.textDecoration = "line-through";
-                item.style.backgroundColor = "black";
-                item.style.color = "white";
-            }
-        })
-    })
